dec7b8ce09e8c389f09abadb17d06adbcf1945ec
Jean <99772124+jeanmends@users.noreply.github.com>
2025-01-09 21:04:29 -0300

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newText = '';
+    for(let i = string.length - 1; i >= 0; i--){
+        newText+= string[i];
+    }
 
+    return newText;
 };
 
 // Do not edit below this line
