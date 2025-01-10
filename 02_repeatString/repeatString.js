cc92c02d8243840b19abdcf9b8ff7a949835d6e7
Jean <99772124+jeanmends@users.noreply.github.com>
2025-01-09 21:00:19 -0300

    exercise number two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let newText = '';
+    if (num < 0){
+        return "ERROR";
+    }
 
+    for (let i = 0; i < num; i++){
+        newText+= string;
+    }
+
+    return newText;
 };
 
 // Do not edit below this line
